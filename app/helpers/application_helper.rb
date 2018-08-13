364fed0c4118876e992a1b32422dd548a5d1ee4b
Richard Roepke <RAERoepke@gmail.com>
2018-08-13 16:36:41 -0400

    Using More Bootstrap Styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def generate_title (page_title)
+    if page_title.present?
+      return 'Campground Compare: ' + page_title
+    else
+      return 'Campground Compare'
+    end
+  end
 end
