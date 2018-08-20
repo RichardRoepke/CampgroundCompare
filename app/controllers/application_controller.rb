16aaa2543d55e749e5662011d327cd6dbbeaeb6e
Richard Roepke <RAERoepke@gmail.com>
2018-08-20 16:03:26 -0400

    Preparing to Render Individual Park

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,4 +17,36 @@ class ApplicationController < ActionController::Base
       end
     end
   end
+
+  def hash_string_to_sym(input_hash)
+    result_hash = {}
+
+    input_hash.each do |sym, value|
+      if value.is_a?(Hash)
+        result_hash[sym.to_sym] = hash_string_to_sym(value)
+      elsif value.is_a?(Array)
+        result_hash[sym.to_sym] = array_string_to_sym(value)
+      else
+        result_hash[sym.to_sym] = value
+      end
+    end
+
+    return result_hash
+  end
+
+  def array_string_to_sym(input_hash)
+    result_hash = []
+
+    input_hash.each do |value|
+      if value.is_a?(Hash)
+        result_hash.push hash_string_to_sym(value)
+      elsif value.is_a?(Array)
+        result_hash.push array_string_to_sym(value)
+      else
+        result_hash.push value
+      end
+    end
+
+    return result_hash
+  end
 end
--- app/controllers/marked_park_controller.rb
+++ app/controllers/marked_park_controller.rb
@@ -5,8 +5,23 @@ class MarkedParkController < ApplicationController
     @parks = MarkedPark.page(params[:page]).per(12)
   end
 
+  def show
+    park = MarkedPark.find(params[:id])
+    @park = get_individual_park(park.uuid)
+  end
+
   private
   def provide_title
     @title = 'Parks'
+  end
+
+  def get_individual_park(uuid)
+    request = Typhoeus::Request.get('https://centralcatalogue.com/api/v1/locations/' + uuid,
+                                    headers: {'x-api-key' => '3049ae6c-1ba8-463e-a18b-c511fd7ec0b2'},
+                                    :ssl_verifyhost => 0) #Server is set as verified but without proper certification.
+
+    temp_response = JSON.parse(request.response_body)
+
+    return hash_string_to_sym(temp_response)
   end
 end
