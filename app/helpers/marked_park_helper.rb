d3e66ff6dd81ea74bf6aef41dcaeb882039003a5
Richard Roepke <RAERoepke@gmail.com>
2018-08-21 12:31:29 -0400

    Refactor Collapse Generation

--- app/helpers/marked_park_helper.rb
+++ app/helpers/marked_park_helper.rb
@@ -1,2 +1,74 @@
 module MarkedParkHelper
+
+  def collapse_button_generator(body)
+    url = '#' + body.downcase + 'Collapse'
+    controls = body.downcase + 'Collapse'
+
+    link_to(body, url, { class: 'btn btn-secondary',
+                         'data-toggle'.to_sym => "collapse",
+                         role: "button",
+                         'aria-expanded'.to_sym => "false",
+                         'aria-controls'.to_sym => controls })
+  end
+
+  def collapse_generator(name, body_function, array)
+    content_tag(:div, { class: 'collapse', id: name.downcase + 'Collapse' }) do
+      content_tag(:div, class: 'card card-body') do
+        content_tag(:ul, class: 'list-group') do
+          array.each do |item|
+            concat(content_tag(:li, class: 'list-group-item') do
+              # content_tag seems to generate tags where it was called, so the
+              # use of Proc and .call is needed to get it to appear in the
+              # proper location.
+              body_function.call(item)
+            end)
+          end
+        end
+      end
+    end
+  end
+
+  def generate_amenities_entry
+    return Proc.new do |amenity|
+      puts 'FUNCTION: ' + amenity.id.to_s
+      concat(amenity.id.to_s + ': ' + amenity.name + ': ' + amenity.group)
+      concat(tag('br'))
+
+      if amenity.token.present?
+        concat('Trip Adviser Token: ' + amenity.token)
+        concat(tag('br'))
+      end
+
+     if amenity.description.present?
+        concat('Description: ' + amenity.description)
+        concat(tag('br'))
+     end
+    end
+  end
+
+  def generate_cobrands_entry
+    return Proc.new do |cobrand|
+      concat(cobrand.id.to_s + ': ' + cobrand.name)
+      concat(' (' + cobrand.short + ')') if cobrand.short.present?
+      concat(' (' + cobrand.code + ')') if cobrand.code.present?
+      concat(tag('br'))
+
+      if cobrand.description.present?
+        concat('Description: ' + cobrand.description)
+        concat(tag('br'))
+      end
+
+      if cobrand.notes.present?
+        concat('Notes: ' + cobrand.notes)
+        concat(tag('br'))
+      end
+    end
+  end
+
+  def generate_images_entry
+    return Proc.new do |image|
+      concat(image.id)
+      concat(tag('br'))
+    end
+  end
 end
