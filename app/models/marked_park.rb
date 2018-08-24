fce08b1bfdc4ba4b61d6acc572d9a424e7930162
Richard Roepke <RAERoepke@gmail.com>
2018-08-24 13:32:27 -0400

    Reformat Web Requests

--- app/models/marked_park.rb
+++ app/models/marked_park.rb
@@ -14,44 +14,57 @@ class MarkedPark < ApplicationRecord
 
     if catalogue_input.blank?
       puts 'CHECKING FOR CATALOGUE'
-      request = Typhoeus::Request.get('http://centralcatalogue.com:3200/api/v1/locations/' + self.uuid.to_s,
-                                      headers: { 'x-api-key' => '3049ae6c-1ba8-463e-a18b-c511fd7ec0b2' },
-                                      :ssl_verifyhost => 0) #Server is set as verified but without proper certification.
-      if request.response_code == 200
-        temp_response = JSON.parse(request.response_body)
-
-        catalogue_input = hash_string_to_sym(temp_response)
-      else
-        self.status = 'Improper UUID' if request.response_code > 300 && request.response_code < 400
-        puts 'Catalogue failed. ' + request.response_code.to_s
-        inputs = false
-      end
+      catalogue_input = get_catalogue_data(self.uuid)
     end
 
-    catalogue = CatalogueLocationValidator.new(catalogue_input) if inputs.present?
+    catalogue = CatalogueLocationValidator.new(catalogue_input) if catalogue_input.present?
 
     if rvparky_input.blank?
       puts 'Checking RVParky.'
-      request = Typhoeus::Request.get('https://www.rvparky.com/_ws2/Location/' + self.slug.to_s.strip,
-                                      :ssl_verifyhost => 0) #Server is set as verified but without proper certification.
-      if request.response_code == 200
-        temp_response = JSON.parse(request.response_body)
-
-        rvparky_input = hash_string_to_sym(temp_response)
-      else
-        self.status = 'Improper slug' if request.response_code > 300 && request.response_code < 400
-        puts 'RVParky failed. ' + request.response_code.to_s
-        inputs = false
-      end
+      rvparky_input = get_rvparky_data(self.slug)
     end
 
-    rvparky = RvparkyLocationValidator.new(rvparky_input[:location]) if inputs.present?
+    rvparky = RvparkyLocationValidator.new(rvparky_input) if rvparky_input.present?
 
-    if inputs.present?
+    if rvparky.present? && catalogue.present?
       self.status = calculate_status(catalogue, rvparky)
+    elsif rvparky.present?
+      self.status = 'UUID IS INVALID'
+    elsif catalogue.present?
+      self.status = 'SLUG IS INVALID'
     else
-      self.status = 'No Connection' unless self.status.present?
+      self.status = 'NO CONNECTION' unless self.status.present?
     end
+  end
+
+  def get_catalogue_data(rv_uuid)
+    return get_web_data('http://centralcatalogue.com:3200/api/v1/locations/' + rv_uuid.to_s,
+                        true)
+  end
+
+  def get_rvparky_data(rv_slug)
+    temp = get_web_data('https://www.rvparky.com/_ws2/Location/' + rv_slug.to_s)
+    return temp[:location] if temp.present?
+    return nil
+  end
+
+  def get_web_data(url, api_key=false)
+    output = nil
+
+    if api_key.present?
+      request = Typhoeus::Request.get(url,
+                                      headers: { 'x-api-key' => '3049ae6c-1ba8-463e-a18b-c511fd7ec0b2' },
+                                      :ssl_verifyhost => 0) #Server is set as verified but without proper certification.
+    else
+      request = Typhoeus::Request.get(url, :ssl_verifyhost => 0) #Server is set as verified but without proper certification.
+    end
+
+    if request.response_code == 200
+      temp_response = JSON.parse(request.response_body)
+      output = hash_string_to_sym(temp_response)
+    end
+
+    return output
   end
 
   def calculate_status(catalogue, rvparky)
