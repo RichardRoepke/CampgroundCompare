a067eff1a7ec7440ae3e95b02b51811ff386fea6
Richard Roepke <RAERoepke@gmail.com>
2018-09-05 12:18:37 -0400

    Replace Session with Coffeescript Validation
    Coffeescript is used to validate forms before they are submitted, instead of validating them afterwards and reloading the form if invalid.

--- app/assets/javascripts/marked_park.coffee
+++ app/assets/javascripts/marked_park.coffee
@@ -11,19 +11,42 @@ $ ->
         origin = $(element).data("mirror")
         foundDestination = document.getElementById(destination)
         foundOrigin = document.getElementById(origin)
-        if !foundDestination.value
+        if !foundDestination.value || $(this).data("global") != "[data-blank]"
           foundDestination.value = foundOrigin.value
+        validateField(foundDestination.value, foundOrigin.value, $(foundDestination).data("row"))
       else
         token = $(element).data("element")
         foundElement = document.getElementById(token)
         foundElement.value = $(element).data("transfer")
+        validateField(foundElement.value, $(element).context.value, $(foundElement).data("row"))
   $("input[data-copy]").click ->
     destination = $(this).data("element")
     origin = $(this).data("mirror")
     foundDestination = document.getElementById(destination)
     foundOrigin = document.getElementById(origin)
     foundDestination.value = foundOrigin.value
+    validateField(foundDestination.value, foundOrigin.value, $(foundDestination).data("row"))
   $("input[data-transfer]").click ->
     token = $(this).data("element")
     foundElement = document.getElementById(token)
     foundElement.value = $(this).data("transfer")
+    validateField(foundElement.value, $(this).context.value, $(foundElement).data("row"))
+  $("input[data-submit]").click (event) ->
+    elements = document.querySelectorAll("tr")
+    for element in elements
+      if element.id != ''
+        if !element.classList.contains("table-success")
+          event.preventDefault()
+  $("textarea").focusout ->
+    mirror = document.getElementById($(this).data("mirror"))
+    validateField(mirror.value, $(this).context.value, $(this).data("row"))
+
+validateField =(firstValue, secondValue, rowID) ->
+  row = document.getElementById(rowID)
+  if firstValue != secondValue
+      row.classList.remove("table-warning", "table-success")
+      row.classList.add("table-danger")
+    else
+      row.classList.remove("table-warning", "table-danger")
+      row.classList.add("table-success")
+      
--- app/controllers/marked_park_controller.rb
+++ app/controllers/marked_park_controller.rb
@@ -3,9 +3,6 @@ class MarkedParkController < ApplicationController
   before_action :provide_title
 
   def index
-    # Cleaning up the session.
-    session[:previous_edit] = nil if session[:previous_edit].present?
-
     @parks = MarkedPark.page(params[:page]).per(12)
   end
 
@@ -68,17 +65,6 @@ class MarkedParkController < ApplicationController
     if @park.editable?
       @catalogue = nil
       @rvparky = nil
-      @previous_values = nil
-
-      # I have no idea why :id turns into 'id' in the session, but otherwise
-      # rails won't recognize the presence of a ID.
-      if session[:previous_edit].present?
-        if session[:previous_edit]['id'] == @park.id
-          @previous_values = session[:previous_edit]
-        else
-          session[:previous_edit] = nil
-        end
-      end
 
       if @park.uuid.present?
         catalogue_temp = get_catalogue_park(@park.uuid)
@@ -112,84 +98,68 @@ class MarkedParkController < ApplicationController
   def submit_changes
     park = MarkedPark.find(params[:id])
 
-    processed_inputs = validate_changes(params)
+    processed_inputs = process_changes(params)
 
-    if processed_inputs[:status] == 'MATCH'
-      # Incase the user has had to rework a submission more than once.
-      session[:previous_edit] = nil if session[:previous_edit].present?
-      catalogue_changed = ''
+    catalogue_url = calc_catalogue_url(processed_inputs[:catalogue], park)
+    rvparky_url = calc_rvparky_url(processed_inputs[:rvparky], park)
 
-      catalogue_changed = process_catalogue(processed_inputs[:catalogue],
-                                            park) if processed_inputs[:catalogue].present?
+    uuid = '5ac85c35-c512-4ed4-bef1-28118d6c7e9e' # Progressive's uuid. Don't want to accidentially mess something up.
 
-      rvparky_changed = process_rvparky(processed_inputs[:rvparky],
-                                        park) if processed_inputs[:rvparky].present?
-
-      uuid = '5ac85c35-c512-4ed4-bef1-28118d6c7e9e' # Progressive's uuid. Don't want to accidentially mess something up.
-
-      catalogue_message = { status: 'CAT NONE',
-                            message: '' }
-      rvparky_message = { status: 'RV NONE',
+    catalogue_message = { status: 'CAT NONE',
                           message: '' }
+    rvparky_message = { status: 'RV NONE',
+                        message: '' }
 
-      if catalogue_changed.present?
-        request = Typhoeus::Request.put('http://centralcatalogue.com:3200/api/v1/locations/' + uuid + '?' + catalogue_changed,
-                                        headers: {'x-api-key' => '3049ae6c-1ba8-463e-a18b-c511fd7ec0b2'},
-                                        :ssl_verifyhost => 0) #Server is set as verified but without proper certification.
-        if request.response_code == 201
-          catalogue_message[:status] = 'CAT SUCCESS'
-          catalogue_message[:message] = 'Central Catalogue: Changes successfully submitted.'
-        else
-          catalogue_message[:status] = 'CAT ALERT'
-          catalogue_message[:message] = 'Central Catalogue: There was an error submitting the changes. Please try again shortly.'
-        end
-      end
-
-      if rvparky_changed.present?
-        if true # request.response_code == 201
-          rvparky_message[:status] = 'RV SUCCESS'
-          rvparky_message[:message] = 'RVParky: Changes successfully submitted.'
-        else
-          rvparky_message[:status] = 'RV ALERT'
-          rvparky_message[:message] = 'RVParky: There was an error submitting the changes. Please try again shortly.'
-        end
-      end
-
-      old_status = park.status
-
-      if catalogue_message[:status].include? 'SUCCESS'
-        park.status = 'BOTH UPDATING' if rvparky_message[:status].include? 'SUCCESS'
-        park.status = 'CATALOGUE UPDATING' if rvparky_message[:status].include? 'NONE'
-      elsif catalogue_message[:status].include? 'NONE'
-        park.status = 'RVPARKY UPDATING' if rvparky_message[:status].include? 'SUCCESS'
-      end
-
-      park.editable = old_status == park.status
-      park.save
-
-      flash[catalogue_message[:status]] = catalogue_message[:message] unless catalogue_message[:status].include?('NONE')
-      flash[rvparky_message[:status]] = rvparky_message[:message] unless rvparky_message[:status].include?('NONE')
-
-      if params["commit"] == 'Submit and Next'
-        target = park
-
-        loop do
-          target = target.next
-          break if target.blank? || target.editable?
-        end
-
-        redirect_to marked_park_quick_path(target.id) if target.present?
-        redirect_to marked_park_index_path, alert: 'No further parks found.' unless target.present?
+    if catalogue_url.present?
+      request = Typhoeus::Request.put('http://centralcatalogue.com:3200/api/v1/locations/' + uuid + '?' + catalogue_url,
+                                      headers: {'x-api-key' => '3049ae6c-1ba8-463e-a18b-c511fd7ec0b2'},
+                                      :ssl_verifyhost => 0) #Server is set as verified but without proper certification.
+      if request.response_code == 201
+        catalogue_message[:status] = 'CAT SUCCESS'
+        catalogue_message[:message] = 'Central Catalogue: Changes successfully submitted.'
       else
-        redirect_to marked_park_index_path
+        catalogue_message[:status] = 'CAT ALERT'
+        catalogue_message[:message] = 'Central Catalogue: There was an error submitting the changes. Please try again shortly.'
       end
+    end
+
+    if rvparky_url.present?
+      if true # request.response_code == 201
+        rvparky_message[:status] = 'RV SUCCESS'
+        rvparky_message[:message] = 'RVParky: Changes successfully submitted.'
+      else
+        rvparky_message[:status] = 'RV ALERT'
+        rvparky_message[:message] = 'RVParky: There was an error submitting the changes. Please try again shortly.'
+      end
+    end
+
+    old_status = park.status
+
+    if catalogue_message[:status].include? 'SUCCESS'
+      park.status = 'BOTH UPDATING' if rvparky_message[:status].include? 'SUCCESS'
+      park.status = 'CATALOGUE UPDATING' if rvparky_message[:status].include? 'NONE'
+    elsif catalogue_message[:status].include? 'NONE'
+      park.status = 'RVPARKY UPDATING' if rvparky_message[:status].include? 'SUCCESS'
+    end
+
+    park.editable = (old_status == park.status)
+    park.save
+
+    flash[catalogue_message[:status]] = catalogue_message[:message] unless catalogue_message[:status].include?('NONE')
+    flash[rvparky_message[:status]] = rvparky_message[:message] unless rvparky_message[:status].include?('NONE')
+
+    if params["commit"] == 'Submit and Next'
+      target = park
+
+      loop do
+        target = target.next
+        break if target.blank? || target.editable?
+      end
+
+      redirect_to marked_park_quick_path(target.id) if target.present?
+      redirect_to marked_park_index_path, alert: 'No further parks found.' unless target.present?
     else
-      # Using the session to transfer previous inputs because I have no other clue
-      # how to do so.
-      # session[:previous_edit] = processed_inputs
-      # session[:previous_edit][:id] = park.id
-      flash[:WARNING] = 'Field mismatch. Please double-check that all values are the same on both sides.'
-      redirect_back fallback_location: root_path
+      redirect_to marked_park_index_path
     end
   end
 
@@ -270,12 +240,11 @@ class MarkedParkController < ApplicationController
     return output
   end
 
-  def validate_changes(inputs)
+  def process_changes(form_inputs)
     result = { catalogue: {},
-               rvparky: {},
-               status: 'MATCH' }
+               rvparky: {} }
 
-    inputs.each do |key, value|
+    form_inputs.each do |key, value|
       if key.to_s.include?('Catalogue_')
         cut_string = key.to_s.remove('Catalogue_')
         result[:catalogue][cut_string.to_sym] = value
@@ -285,15 +254,10 @@ class MarkedParkController < ApplicationController
       end
     end
 
-    # common_fields is taken from the CommonFields module.
-    common_fields.each do |cat, rv|
-      result[:status] = 'MISMATCH' unless result[:catalogue][cat.to_sym] == result[:rvparky][rv.to_sym]
-    end
-
     return result
   end
 
-  def process_catalogue(catalogue_hash, park)
+  def calc_catalogue_url(catalogue_hash, park)
     result = ''
 
     catalogue_hash.each do |key, value|
@@ -309,7 +273,7 @@ class MarkedParkController < ApplicationController
     return result
   end
 
-  def process_rvparky(rvparky_hash, park)
+  def calc_rvparky_url(rvparky_hash, park)
     return 'foobar' # Once the API for updating RVParky is known, this will be filled in.
   end
 
