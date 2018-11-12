b7c9394bcc46a604c80428fa9c8bcc4814db79fa
Richard Roepke <RAERoepke@gmail.com>
2018-11-12 10:03:46 -0500

    Temporary Database Change

--- db/migrate/20181112144536_add_rvparky_id_to_marked_park.rb
+++ db/migrate/20181112144536_add_rvparky_id_to_marked_park.rb
@@ -1,4 +1,4 @@
-class AddRvparkyIDtoMarkedPark < ActiveRecord::Migration[5.2]
+class AddRvparkyIdToMarkedPark < ActiveRecord::Migration[5.2]
   def change
     add_column :marked_park, :rvparky_id, :integer
   end
