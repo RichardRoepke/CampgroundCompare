f803106a3a6b37cc73481d2e963481b4ea60f080
Richard Roepke <RAERoepke@gmail.com>
2018-11-20 13:45:20 -0500

    Update Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,7 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-User.create!({ email: 'richard@missionmgmt.com',
-               password: 'asdfasdf',
-               password_confirmation: 'asdfasdf',
+User.create!({ email: 'admin@missionmgmt.com',
+               password: 'changeThis',
+               password_confirmation: 'changeThis',
                admin: true})
-User.create!({ email: 'admin@admin.org',
-               password: 'asdfasdf',
-               password_confirmation: 'asdfasdf',
-               admin: true})
-User.create!({ email: 'user@user.org',
-               password: 'asdfasdf',
-               password_confirmation: 'asdfasdf',
-               admin: false})
-
-10.times do |num|
-  mail = (0...(3 + rand(12))).map { (65 + rand(26)).chr }.join
-  User.create!({email: 'SEED_' + mail + '@jibberish.org',
-                password: 'asdfasdf',
-                password_confirmation: 'asdfasdf',
-                admin: false})
 end
